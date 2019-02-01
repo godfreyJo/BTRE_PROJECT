09d8d46ad811c83346daabfdcc94fb3151b822e6
godfreyJo <godfreyoyugi@gmail.com>
2019-02-01 14:39:48 +0300

    adding the connection to database feature to the frontend

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from listings.choices import price_choices, bedroom_choices, state_choices
+
+from listings.models import Listing
+from realtors.models import Realtor
 
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'pages/index.html')
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+	context = {
+		'listings': listings,
+	    'state_choices': state_choices,
+	    'bedroom_choices': bedroom_choices,
+	    'price_choices': price_choices
+	}
+		
+	    
+		
+
+	return render(request, 'pages/index.html', context)
 
 
 def about(request):
